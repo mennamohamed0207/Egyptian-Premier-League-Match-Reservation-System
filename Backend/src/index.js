22fae4caa0116ceb425d362388fd365b1dc3a071
fatmaebrahim <fatmaebrahim3131@gmail.com>
2024-12-07 19:01:12 +0200

    fix: added server link

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -19,7 +19,7 @@ app.use('/', authRoutes);
 connectToDatabase()
   .then(() => {
     app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+      console.log(`Server is running on http://localhost:${port}`);
     });
   })
   .catch((err) => {

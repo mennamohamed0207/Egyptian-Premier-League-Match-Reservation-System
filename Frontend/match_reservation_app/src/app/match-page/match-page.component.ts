0452ca9ab968550f6389e5fb70090f48d09581f9
Sara Bisheer <113303945+SH8664@users.noreply.github.com>
2024-12-12 08:26:24 +0200

    feat: update Match Page component

--- Frontend/match_reservation_app/src/app/match-page/match-page.component.ts
+++ Frontend/match_reservation_app/src/app/match-page/match-page.component.ts
@@ -12,6 +12,9 @@ export class MatchPageComponent {
   rows: number = 3;
   cols: number = 5;
 
+  available= '#ecf39eff'; 
+  reserved= '#31572cff';   
+  selected= 'rgb(137, 224, 60)';   
   chairs_colors : string[][] = [];
   reserved_chairs : number[][] = [[0,1,1,0,1],[0,0,0,1,1],[1,0,1,0,1]];
   constructor() {
@@ -19,18 +22,17 @@ export class MatchPageComponent {
       let chairs_row : string[] = [];
       for (let j = 0; j < this.cols; j++) {
         if (this.reserved_chairs[i][j] == 1 )
-           chairs_row.push('gray');
+           chairs_row.push(this.reserved);
         else
-           chairs_row.push('white');
+           chairs_row.push(this.available);
           
       }
       this.chairs_colors.push(chairs_row);
     }
   }
   changeColor(i:number,j:number) {
-    if (this.chairs_colors[i][j] == 'gray')
-      return;
+    if (this.chairs_colors[i][j] == this.reserved) return;
     this.chairs_colors[i][j] =
-      this.chairs_colors[i][j] == 'white' ? 'green' : 'white';
+      this.chairs_colors[i][j] == this.available ? this.selected : this.available;
   }
 }

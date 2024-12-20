79d08e86411491f292a20c327992476561d58a4a
mennamohamed0207 <menamohamed0207@gmail.com>
2024-12-20 23:45:39 +0200

    adding admin apis

--- Frontend/match_reservation_app/src/app/admin/admin.component.ts
+++ Frontend/match_reservation_app/src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app-admin',
@@ -7,45 +9,60 @@ import { Component } from '@angular/core';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css', '../app.component.css'],
 })
-export class AdminComponent {
-  allUsers = [
-    { id: 1, email: 'user1@example.com', isManager: false },
-    { id: 2, email: 'user2@example.com', isManager: false },
-    { id: 3, email: 'user3@example.com', isManager: false },
-    { id: 4, email: 'user4@example.com', isManager: true },
-    { id: 5, email: 'user5@example.com', isManager: false },
-    { id: 1, email: 'user1@example.com', isManager: false },
-    { id: 2, email: 'user2@example.com', isManager: false },
-    { id: 3, email: 'user3@example.com', isManager: false },
-    { id: 4, email: 'user4@example.com', isManager: true },
-    { id: 5, email: 'user5@example.com', isManager: false },
-    { id: 1, email: 'user1@example.com', isManager: false },
-    { id: 2, email: 'user2@example.com', isManager: false },
-    { id: 3, email: 'user3@example.com', isManager: false },
-    { id: 4, email: 'user4@example.com', isManager: true },
-    { id: 5, email: 'user5@example.com', isManager: false },
-    { id: 1, email: 'user1@example.com', isManager: false },
-    { id: 2, email: 'user2@example.com', isManager: false },
-    { id: 3, email: 'user3@example.com', isManager: false },
-    { id: 4, email: 'user4@example.com', isManager: true },
-    { id: 5, email: 'user5@example.com', isManager: false },
-  ];
+export class AdminComponent implements OnInit {
+  allUsers: {
+    _id: string;
+    username: string;
+    email: string;
+    role: string;
+    status: string;
 
-  addAuthority(userId: number) {
-    const user = this.allUsers.find((u) => u.id === userId);
+  } | any = [];
+  constructor(private dataservice: UserService) {
+  }
+  ngOnInit(): void {
+    this.dataservice.getAllusers().subscribe((data) => {
+      console.log(data);
+      this.allUsers = data['users'];
+    });
+  }
+  addAuthority(userId: string) {
+    const user = this.allUsers.find((u: { _id: string }) => u._id === userId);
+    console.log(user);
+    
     if (user) {
       user.isManager = true;
+      /*{
+  "customerUsername": "string",
+  "status": "Pending"
+} */
+      const form: any = {
+        "customerUsername": user.username,
+        "status": user.status
+      }
+      console.log(form);
+      this.dataservice.approveUser(user.username, form).subscribe((data) => {
+        console.log("heeeeeeeeeeeeeeeeeeeeeer")
+        console.log(data);
+        user.role = "Manager";
+      }
+      );
     }
   }
 
   removeAuthority(userId: number) {
-    const user = this.allUsers.find((u) => u.id === userId);
+    const user = this.allUsers.find((u: { id: number }) => u.id === userId);
     if (user) {
       user.isManager = false;
     }
   }
 
-  removeUserAccount(userId: number) {
-    this.allUsers = this.allUsers.filter((u) => u.id !== userId);
+  removeUserAccount(userId: string) {
+    const user = this.allUsers.find((u: { _id: string }) => u._id === userId);
+    this.allUsers = this.allUsers.filter((u: { _id: string }) => u._id !== userId);
+
+    this.dataservice.deleteUser(user.username).subscribe((data) => {
+      console.log(data);
+    });
   }
 }
--- Frontend/match_reservation_app/src/app/services/user.service.ts
+++ Frontend/match_reservation_app/src/app/services/user.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {  Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private apiUrl = '/user';
+  private apiUrl = 'http://localhost:3000/user';
 
   constructor(private http: HttpClient) { }
 
@@ -35,7 +36,7 @@ export class UserService {
   }
   getUser(username: any): Observable<any> {
 
-    const token = localStorage.getItem('accessToken'); // Replace with your token retrieval logic
+    const token = typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null; // Replace with your token retrieval logic
     console.log(token);
 
     const headers = new HttpHeaders().set('JWT-Token', `${token}`).set('Content-Type', 'application/json');
@@ -51,12 +52,39 @@ export class UserService {
     return this.http.get(`http://localhost:3000/ticket`, { headers });
   }
   updateProfile(username: any, data: any): Observable<any> {
-    const token = localStorage.getItem('accessToken'); // Replace with your token retrieval logic
+    const token = typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null; // Replace with your token retrieval logic
     console.log(token);
     console.log("user");
     console.log(data);
     const headers = new HttpHeaders().set('JWT-Token', `${token}`).set('Content-Type', 'application/json');
 
     return this.http.put(`http://localhost:3000/user/${username}/edit`, data, { headers });
+  }
+  getAllusers(): Observable<any> {
+    const token = typeof window !== 'undefined' ? localStorage.getItem('accessToken') : null; // Replace with your token retrieval logic
+    console.log(token);
+    const username = typeof window !== 'undefined' && localStorage ? localStorage.getItem('username') : null;
+    const headers = new HttpHeaders().set('JWT-Token', `${token}`).set('Content-Type', 'application/json');
+
+    return this.http.get(`http://localhost:3000/user/${username}/users`, { headers });
+  }
+  approveUser(user: any, form: any): Observable<any> {
+    const token = localStorage.getItem('accessToken'); // Replace with your token retrieval logic
+    console.log(token);
+    const username = localStorage.getItem('username');
+    console.log(username);
+    console.log(form)
+    const headers = new HttpHeaders().set('JWT-Token', `${token}`).set('Content-Type', 'application/json');
+    return this.http.put(`http://localhost:3000/user/${username}/`,  form , { headers });
+  }
+  deleteUser(user:any): Observable<any> {
+    const token = localStorage.getItem('accessToken'); // Replace with your token retrieval logic
+    console.log(token);
+    const username = localStorage.getItem('username');
+
+    console.log(username);
+
+    const headers = new HttpHeaders().set('JWT-Token', `${token}`).set('Content-Type', 'application/json');
+    return this.http.delete(`http://localhost:3000/user/${username}`, { headers, body: { customerUsername: user } });
   }
 }
